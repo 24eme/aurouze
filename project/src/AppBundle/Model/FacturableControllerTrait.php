39f1ec1a39cf4be15776eef072f005d82d0503bb
Honorine-MC <honorine.mariechristy@gmail.com>
2020-10-20 12:24:23 +0200

    ajout de la clé dans l'url du pdf + ajout paramètres dans toutes les vues

--- project/src/AppBundle/Model/FacturableControllerTrait.php
+++ project/src/AppBundle/Model/FacturableControllerTrait.php
@@ -10,132 +10,141 @@ use Symfony\Component\HttpFoundation\Response;
 trait FacturableControllerTrait
 {
     /**
-     * @Route("/{document}/pdf", name="facturable_pdf")
+     * @Route("/{document}/pdf/{key}", name="facturable_pdf")
      */
     public function pdfAction(Request $request, $document)
     {
+
         $type = strtolower(strtok($document, '-'));
         $manager = $this->get($type.'.manager');
         $repository = $manager->getRepository('AppBundle:'.ucfirst($type));
         $document = $repository->findOneById($document);
 
-        if (! $document instanceof FacturableInterface) {
-            throw new \Exception($type." n'est pas de type FacturableInterface");
+       if($document->getSecretKey()== hash ('sha256' , $document->getId().$this->getSecretKey())){
+            if (! $document instanceof FacturableInterface) {
+                throw new \Exception($type." n'est pas de type FacturableInterface");
+            }
+            $devisLie = null;
+            if($type == "facture"){
+              $dm = $this->get('devis.manager');
+              $devisLie = $dm->findByFacture($document);
+            }
+
+            $pages = [];
+
+            $nbLigneMaxPourPageVierge = 50;
+            $nbLigneMaxPourDernierePage = 30;
+            $nbPage = 1;
+            $nbMaxCharByLigne = 60;
+            $nbCurrentLigne = 0;
+            $nbCurrentPage = 1;
+            $nbLigneParLigneFacture = [];
+            $nbLigneParPage = [1 => $nbLigneMaxPourDernierePage];
+
+            foreach ($document->getLignes() as $key => $ligne) {
+                $nbCurrentLigne += 2;
+                if($ligne->getReferenceClient()) {
+                    $nbCurrentLigne += 1;
+                }
+
+                if($ligne->isOrigineContrat()) {
+                    $nbCurrentLigne += 4;
+                    $nbCurrentLigne += count($ligne->getOrigineDocument()->getPrestations());
+                    $nbCurrentLigne += count($ligne->getOrigineDocument()->getContratPassages());
+                }
+
+                $nbLigneParLigneFacture[$key] = $nbCurrentLigne;
+
+                if($nbCurrentPage == $nbPage && $nbCurrentLigne > $nbLigneMaxPourDernierePage) {
+                    $nbLigneParPage[$nbCurrentPage] = $nbLigneMaxPourDernierePage;
+                    $nbPage += 1;
+                    $nbLigneParPage[$nbPage] = $nbLigneMaxPourDernierePage;
+                }
+
+                if($nbCurrentPage < $nbPage && $nbCurrentLigne > $nbLigneMaxPourPageVierge) {
+                    $nbLigneParPage[$nbCurrentPage] = $nbLigneMaxPourPageVierge;
+                    $nbCurrentPage += 1;
+                    $nbCurrentLigne = 0;
+                }
+
+            }
+
+            $nbCurrentPage = 1;
+            $nbCurrentLigne = 0;
+            foreach($document->getLignes() as $key => $ligneFacture) {
+
+                $ligne = $this->buildLignePDFFacture($ligneFacture);
+
+                // La ligne ne tient pas sur une page complète
+                if(($nbLigneParLigneFacture[$key]) > $nbLigneParPage[$nbCurrentPage]) {
+                    $nbLignes2Keep = (int)(0.8 * $nbLigneParPage[$nbCurrentPage]);
+                    $lignesSplitted = $this->splitLigne($ligne, $nbLignes2Keep);
+                    $pages[$nbCurrentPage][] = $lignesSplitted[0];
+                    $pages[$nbCurrentPage+1][] = $lignesSplitted[1];
+                    $nbCurrentLigne = 0;
+                    $nbCurrentPage += 1;
+                    continue;
+                }
+
+                // La ligne tient sur la page
+                if(($nbCurrentLigne + $nbLigneParLigneFacture[$key]) < $nbLigneParPage[$nbCurrentPage]) {
+
+                    $pages[$nbCurrentPage][] = $ligne;
+                    continue;
+                }
+
+                // La ligne ne tient plus sur la page
+                if(($nbCurrentLigne + $nbLigneParLigneFacture[$key]) > $nbLigneParPage[$nbCurrentPage]) {
+
+                    $nbCurrentLigne = 0;
+                    $nbCurrentPage += 1;
+                    $pages[$nbCurrentPage][] = $ligne;
+                    continue;
+                }
+            }
+
+            $html = $this->renderView($type.'/pdf.html.twig', array(
+                $type => $document,
+                'devisLie' => $devisLie,
+                'pages' => $pages,
+                'parameters' => $manager->getParameters(),
+            ));
+
+            if ($request->get('output') == 'html') {
+
+                return new Response($html, 200);
+            }
+
+            $suffix = ($document->getNumero()) ? 'N'.$document->getNumero()
+                                               : 'brouillon';
+
+            if ($document->getDocumentType() === Facture::DOCUMENT_TYPE && $document->isAvoir()) {
+                $type = 'avoir';
+            }
+
+            $filename = implode('_', [
+                $type,
+                $document->getSociete()->getIdentifiant(),
+                $document->getDateEmission()->format('Ymd'),
+                $document->getNumero(),
+                $document->getSecretKey(),
+                hash ('sha256' , $document->getId().$this->getSecretKey()),
+                $suffix
+            ]);
+            $filename .= '.pdf';
+
+            return new Response(
+                    $this->get('knp_snappy.pdf')->getOutputFromHtml($html, $this->getPdfGenerationOptions()), 200, array(
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'attachment; filename="' . $filename . '"',
+                    )
+            );
+      }
+        else{
+            throw $this->createNotFoundException();
         }
-        $devisLie = null;
-        if($type == "facture"){
-          $dm = $this->get('devis.manager');
-          $devisLie = $dm->findByFacture($document);
-        }
-
-        $pages = [];
-
-        $nbLigneMaxPourPageVierge = 50;
-        $nbLigneMaxPourDernierePage = 30;
-        $nbPage = 1;
-        $nbMaxCharByLigne = 60;
-        $nbCurrentLigne = 0;
-        $nbCurrentPage = 1;
-        $nbLigneParLigneFacture = [];
-        $nbLigneParPage = [1 => $nbLigneMaxPourDernierePage];
-
-        foreach ($document->getLignes() as $key => $ligne) {
-            $nbCurrentLigne += 2;
-            if($ligne->getReferenceClient()) {
-                $nbCurrentLigne += 1;
-            }
-
-            if($ligne->isOrigineContrat()) {
-                $nbCurrentLigne += 4;
-                $nbCurrentLigne += count($ligne->getOrigineDocument()->getPrestations());
-                $nbCurrentLigne += count($ligne->getOrigineDocument()->getContratPassages());
-            }
-
-            $nbLigneParLigneFacture[$key] = $nbCurrentLigne;
-
-            if($nbCurrentPage == $nbPage && $nbCurrentLigne > $nbLigneMaxPourDernierePage) {
-                $nbLigneParPage[$nbCurrentPage] = $nbLigneMaxPourDernierePage;
-                $nbPage += 1;
-                $nbLigneParPage[$nbPage] = $nbLigneMaxPourDernierePage;
-            }
-
-            if($nbCurrentPage < $nbPage && $nbCurrentLigne > $nbLigneMaxPourPageVierge) {
-                $nbLigneParPage[$nbCurrentPage] = $nbLigneMaxPourPageVierge;
-                $nbCurrentPage += 1;
-                $nbCurrentLigne = 0;
-            }
-
-        }
-
-        $nbCurrentPage = 1;
-        $nbCurrentLigne = 0;
-        foreach($document->getLignes() as $key => $ligneFacture) {
-
-            $ligne = $this->buildLignePDFFacture($ligneFacture);
-
-            // La ligne ne tient pas sur une page complète
-            if(($nbLigneParLigneFacture[$key]) > $nbLigneParPage[$nbCurrentPage]) {
-                $nbLignes2Keep = (int)(0.8 * $nbLigneParPage[$nbCurrentPage]);
-                $lignesSplitted = $this->splitLigne($ligne, $nbLignes2Keep);
-                $pages[$nbCurrentPage][] = $lignesSplitted[0];
-                $pages[$nbCurrentPage+1][] = $lignesSplitted[1];
-                $nbCurrentLigne = 0;
-                $nbCurrentPage += 1;
-                continue;
-            }
-
-            // La ligne tient sur la page
-            if(($nbCurrentLigne + $nbLigneParLigneFacture[$key]) < $nbLigneParPage[$nbCurrentPage]) {
-
-                $pages[$nbCurrentPage][] = $ligne;
-                continue;
-            }
-
-            // La ligne ne tient plus sur la page
-            if(($nbCurrentLigne + $nbLigneParLigneFacture[$key]) > $nbLigneParPage[$nbCurrentPage]) {
-
-                $nbCurrentLigne = 0;
-                $nbCurrentPage += 1;
-                $pages[$nbCurrentPage][] = $ligne;
-                continue;
-            }
-        }
-
-        $html = $this->renderView($type.'/pdf.html.twig', array(
-            $type => $document,
-            'devisLie' => $devisLie,
-            'pages' => $pages,
-            'parameters' => $manager->getParameters(),
-        ));
-
-        if ($request->get('output') == 'html') {
-
-            return new Response($html, 200);
-        }
-
-        $suffix = ($document->getNumero()) ? 'N'.$document->getNumero()
-                                           : 'brouillon';
-
-        if ($document->getDocumentType() === Facture::DOCUMENT_TYPE && $document->isAvoir()) {
-            $type = 'avoir';
-        }
-
-        $filename = implode('_', [
-            $type,
-            $document->getSociete()->getIdentifiant(),
-            $document->getDateEmission()->format('Ymd'),
-            $document->getNumero(),
-            $suffix
-        ]);
-        $filename .= '.pdf';
-
-        return new Response(
-                $this->get('knp_snappy.pdf')->getOutputFromHtml($html, $this->getPdfGenerationOptions()), 200, array(
-            'Content-Type' => 'application/pdf',
-            'Content-Disposition' => 'attachment; filename="' . $filename . '"',
-                )
-        );
     }
+
 
     /**
      * Construit une ligne de facture pour les pdf
@@ -208,5 +217,11 @@ trait FacturableControllerTrait
     public function getPdfGenerationOptions() {
         return array('disable-smart-shrinking' => null, 'encoding' => 'utf-8', 'margin-left' => 3, 'margin-right' => 3, 'margin-top' => 4, 'margin-bottom' => 4);
     }
+
+
+      public function getSecretKey(){
+          return $this->getParameter('secret');
+      }
+
 
 }
