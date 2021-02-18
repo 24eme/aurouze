ed4937b118c086607ad7aa51837c73ddefadd9ad
Jb Lm <lemetayer.jb@gmail.com>
2021-02-18 17:57:32 +0100

    prise en compte des devis dans les retards de factures

--- project/src/AppBundle/Repository/FactureRepository.php
+++ project/src/AppBundle/Repository/FactureRepository.php
@@ -131,21 +131,13 @@ class FactureRepository extends DocumentRepository {
     	return $resultSet;
     }
 
-    public function findFactureRetardDePaiement($dateFactureBasse = null, $dateFactureHaute = null, $nbRelance = null, $societe = null, $commercial = null){
-      $today = new \DateTime();
+    private function makeBaseFactureRetardDePaiement($nbRelance = null, $societe = null)
+    {
       $q = $this->createQueryBuilder();
       $q->field('numeroFacture')->notEqual(null);
       $q->field('cloture')->equals(false);
       $q->field('montantTTC')->gt(0.0);
-      //$q->field('montantAPayer')->gt(0.0);
       $q->field('avoir')->equals(null);
-      $q->field('dateLimitePaiement')->lte($today);
-      if($dateFactureBasse){
-        $q->field('dateFacturation')->gte($dateFactureBasse);
-      }
-      if($dateFactureHaute){
-        $q->field('dateFacturation')->lte($dateFactureHaute);
-      }
       if(!is_null($nbRelance)){
         if($nbRelance > 2){
           $q->field('nbRelance')->gte($nbRelance);
@@ -165,14 +157,38 @@ class FactureRepository extends DocumentRepository {
         	$q->field('societe')->equals($societe);
       	}
         $q->sort('dateFacturation', 'desc')->sort('societe', 'asc');
-        $query = $q->getQuery();
-        $results = $query->execute();
+        return $q;
+    }
+
+    public function findFactureRetardDePaiement($dateFactureBasse = null, $dateFactureHaute = null, $nbRelance = null, $societe = null, $commercial = null){
+      $today = new \DateTime();
+
+      // Factures
+      $qF = $this->makeBaseFactureRetardDePaiement($nbRelance, $societe);
+      $qF->field('dateLimitePaiement')->lte($today);
+      if($dateFactureBasse){
+        $qF->field('dateFacturation')->gte($dateFactureBasse);
+      }
+      if($dateFactureHaute){
+        $qF->field('dateFacturation')->lte($dateFactureHaute);
+      }
+      $resultsFacture = $qF->getQuery()->execute();
+
+      // Devis
+
+      $today->modify("-30 days");
+      $qD = $this->makeBaseFactureRetardDePaiement($nbRelance, $societe);
+      $qD->field('numeroDevis')->notEqual(null);
+      $qD->field('dateFacturation')->lt($today);
+      $resultsDevis = $qD->getQuery()->execute();
+
+      $results = array_merge($resultsFacture->toArray(), $resultsDevis->toArray());
+
+
         $factures = array();
         foreach($results as $facture) {
         	if ($commercial) {
-        		if (!$facture->getContrat()) {
-        			continue;
-        		} else {
+        		if ($facture->getContrat()) {
         			if (!$facture->getContrat()->getCommercial()) {
         				continue;
         			} else {
@@ -182,7 +198,7 @@ class FactureRepository extends DocumentRepository {
         			}
         		}
         	}
-            $factures[$facture->getId()] = $facture;
+          $factures[$facture->getId()] = $facture;
         }
 
         return $factures;
@@ -202,17 +218,7 @@ class FactureRepository extends DocumentRepository {
     }
 
     public function findRetardDePaiementBySociete(Societe $societe, $nbJourSeuil = 0){
-      $jour = new \DateTime();
-      $jour->modify("-".$nbJourSeuil." days");
-      $q = $this->createQueryBuilder();
-      $q->field('numeroFacture')->notEqual(null);
-      $q->field('societe')->equals($societe->getId());
-      $q->field('cloture')->equals(false);
-      $q->field('montantTTC')->gt(0.0);
-      $q->field('avoir')->equals(null);
-      $q->field('dateLimitePaiement')->lt($jour)->sort('dateFacturation', 'asc');
-      $query = $q->getQuery();
-      return $query->execute();
+      return $this->findFactureRetardDePaiement(null, null, null, $societe, null);
     }
 
 }
