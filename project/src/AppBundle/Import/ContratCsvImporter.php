5ca911c272d076c40985ad556f1caff066f72a0f
Vincent LAURENT <vince.laurent@gmail.com>
2021-12-29 23:45:17 +0100

    Import des contrats reconnaissance des sociétés

--- project/src/AppBundle/Import/ContratCsvImporter.php
+++ project/src/AppBundle/Import/ContratCsvImporter.php
@@ -36,9 +36,15 @@ class ContratCsvImporter {
     protected $em;
     protected $sm;
     protected $um;
+    protected $societes;
 
-    const CSV_ID_CONTRAT = 0;
-    const CSV_ID_SOCIETE = 1;
+    //const CSV_ID_CONTRAT = 0;
+    const CSV_ID_SOCIETE = 0;
+    const CSV_ADRESSE_NUMERO = 4;
+    const CSV_ADRESSE_RUE = 5;
+    const CSV_ADRESSE_NOM = 6;
+    const CSV_ADRESSE_CODE_POSTAL = 7;
+    const CSV_ADRESSE_COMMUNE = 8;
     const CSV_ID_COMMERCIAL = 2;
     const CSV_ID_TECHNICIEN = 3;
     const CSV_TYPE_CONTRAT = 4;
@@ -66,32 +72,126 @@ class ContratCsvImporter {
         $this->em = $em;
         $this->sm = $sm;
         $this->um = $um;
+        $this->societes = $this->sm->getRepository()->findAll();
+    }
+
+    public function findSociete($data) {
+        $replaceWord = array(" ", "mme.", "m.", "mr", "mlle", "mme", "madame", "monsieur", "mademoiselle", ".", "-", "'", '"', 'earl', 'sarl', ',', '&', 'sas', 'ets', 'sdc');
+        $raisonSocialeData = str_replace($replaceWord, "", strtolower(preg_replace("/\(.+\)/", "", $data[self::CSV_ID_SOCIETE])));
+        $adresseData = str_replace($replaceWord, "", strtolower($data[self::CSV_ADRESSE_NUMERO].$data[self::CSV_ADRESSE_RUE].$data[self::CSV_ADRESSE_NOM].$data[self::CSV_ADRESSE_CODE_POSTAL].$data[self::CSV_ADRESSE_COMMUNE]));
+        $adresseShortData = str_replace($replaceWord, "", strtolower($data[self::CSV_ADRESSE_CODE_POSTAL].$data[self::CSV_ADRESSE_COMMUNE]));
+        $societes = $this->societes;
+        $societesFind = array();
+        foreach($societes as $societe) {
+            $codeComptableSociete = str_replace($replaceWord, "", strtolower(preg_replace("/\(.+\)/", "", $societe->getCodeComptable())));
+
+            if($raisonSocialeData == $codeComptableSociete) {
+                $societesFind[] = $societe;
+            }
+        }
+
+        if(count($societesFind) == 1) {
+
+            return current($societesFind);
+        }
+
+        if(count($societesFind) > 1) {
+            $societes = $societesFind;
+        }
+
+        $societesFind = array();
+        foreach($societes as $societe) {
+            $raisonSocialeSociete = str_replace($replaceWord, "", strtolower(preg_replace("/\(.+\)/", "", $societe->getRaisonSociale())));
+
+            if($raisonSocialeData == $raisonSocialeSociete) {
+                $societesFind[] = $societe;
+            }
+        }
+
+        if(count($societesFind) == 1) {
+
+            return current($societesFind);
+        }
+
+        if(count($societesFind) > 1) {
+            $societes = $societesFind;
+        }
+
+        $societesFind = array();
+        foreach($societes as $societe) {
+            if(trim($data[self::CSV_ADRESSE_CODE_POSTAL]) == trim($societe->getAdresse()->getCodePostal())) {
+                $societesFind[] = $societe;
+            }
+        }
+
+        if(count($societesFind) == 1) {
+
+            return current($societesFind);
+        }
+
+        if(count($societesFind) > 1) {
+            $societes = $societesFind;
+        }
+
+        $societesFind = array();
+        foreach($societes as $societe) {
+            $adresseShortSociete = str_replace($replaceWord, "", strtolower($societe->getAdresse()->getCodePostal().$societe->getAdresse()->getCommune()));
+
+            if($adresseShortData == $adresseShortSociete) {
+                $societesFind[] = $societe;
+            }
+        }
+
+        if(count($societesFind) == 1) {
+
+            return current($societesFind);
+        }
+
+        if(count($societesFind) > 1) {
+            $societes = $societesFind;
+        }
+
+        $societesFind = array();
+        foreach($societes as $societe) {
+            $adresseSociete = str_replace($replaceWord, "", strtolower($societe->getAdresse()->getAdresse().$societe->getAdresse()->getCodePostal().$societe->getAdresse()->getCommune()));
+
+            if($adresseData == $adresseSociete) {
+                $societesFind[] = $societe;
+            }
+        }
+
+        if(count($societesFind) == 1) {
+
+            return current($societesFind);
+        }
+
+        return null;
     }
 
     public function import($file, OutputInterface $output) {
         $csvFile = new CsvFile($file,1,true);
 
-        $progress = new ProgressBar($output, 100);
-        $progress->start();
+        // $progress = new ProgressBar($output, 100);
+        // $progress->start();
 
         $csv = $csvFile->getCsv();
-        $configuration = $this->dm->getRepository('AppBundle:Configuration')->findConfiguration();
-        $produitsArray = $configuration->getProduitsArray();
+        //$configuration = $this->dm->getRepository('AppBundle:Configuration')->findConfiguration();
+        //$produitsArray = $configuration->getProduitsArray();
 
 
         $i = 0;
         $cptTotal = 0;
         foreach ($csv as $data) {
-            $societe = null;
+            $societe = $this->findSociete($data);
+            if(!$societe) {
+                $output->writeln(sprintf("<error>La societe %s n'a pas été trouvé</error>", $data[self::CSV_ID_SOCIETE]));
+                continue;
+            }
+            //echo $data[self::CSV_ID_SOCIETE].":".count($societes)."\n";
+            continue;
+
             if(trim($data[self::CSV_ID_SOCIETEOLDADRESSEID])){
               $societe = $this->sm->getRepository()->findOneBy(array('identifiantAdresseReprise' => $data[self::CSV_ID_SOCIETEOLDADRESSEID]));
-            }
-            if (!$societe && trim($data[self::CSV_ID_SOCIETE])) {
-              $societe = $this->sm->getRepository()->findOneBy(array('identifiantReprise' => $data[self::CSV_ID_SOCIETE]));
-            }
-            if (!$societe) {
-                $output->writeln(sprintf("<error>La societe %s n'existe pas</error>", $data[self::CSV_ID_SOCIETE]));
-                continue;
             }
 
             $contrat = $this->cm->getRepository()->findOneBy(array('identifiantReprise', $data[self::CSV_ID_CONTRAT]));
@@ -199,7 +299,7 @@ class ContratCsvImporter {
         }
 
         $this->dm->flush();
-        $progress->finish();
+        //$progress->finish();
     }
 
 }
