492612d09b0fe57a09e16ea54cc1b4d3f79c4de8
Vincent LAURENT <vince.laurent@gmail.com>
2022-01-10 01:25:44 +0100

    Import des paiements avec reconnaissance de factures

--- project/bin/import_arbh.sh
+++ project/bin/import_arbh.sh
@@ -9,7 +9,7 @@ echo "AHRB;;;AHRB;;16 RUE ANTOINE LAURENT LAVOISIER;;77480;BRAY SUR SEINE;FRANCE
 cat $DATA_DIR/F_COMPTET.csv | awk -F ";" '{ print $1 ";;;" $3 ";" $10 ";" $11 ";" $12 ";" $13 ";" $15 ";" $17 ";" $68 ";;" $69 ";" $71 ";" $70 ";" $1 ";" $24 ";" $23 ";" $22 ";;;;"  }' >> $DATA_DIR/societes.csv
 
 cat $DATA_DIR/F_DOCLIGNE.csv > $DATA_DIR/factures.csv
-cat $DATA_DIR/F_CREGLEMENT.csv > $DATA_DIR/reglements.csv
+cat $DATA_DIR/F_CREGLEMENT.csv | sed 's|6986 A 6988|6986/6987/6988|' | sed 's|7868 A 7871|7868/7869/7870/7871|' > $DATA_DIR/reglements.csv
 
 cat $DATA_DIR/Factures.txt | sed 's/AHRB/@/' | tr "\n" "#" | tr "@" "\n" | sed -r 's/^.+N° intracommunautaire :FR85443936497##([^#]+)#/\1/' | sed -r 's/#.+NUMERO//' | sed 's/##DATE##/;/' | grep "contrat n" | sed -r 's/#.+contrat n[°\ ?a-z#]*([0-9A-Z\/]+).+/;\1/' > $DATA_DIR/factures_contrats.csv
 
--- project/src/AppBundle/Import/PaiementsCsvImporter.php
+++ project/src/AppBundle/Import/PaiementsCsvImporter.php
@@ -16,6 +16,7 @@ namespace AppBundle\Import;
 use Symfony\Component\Console\Output\OutputInterface;
 use AppBundle\Manager\FactureManager;
 use AppBundle\Manager\PaiementsManager;
+use AppBundle\Manager\SocieteManager;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Behat\Transliterator\Transliterator;
 use AppBundle\Document\Paiements;
@@ -30,6 +31,7 @@ class PaiementsCsvImporter {
     protected $debug = false;
 
     const CSV_REGLEMENT_ID = 0;
+    const CSV_SOCIETE_ID = 1;
     const CSV_PAIEMENT_DATE = 3;
     const CSV_LIBELLE = 5;
     const CSV_MONTANT = 6;
@@ -40,10 +42,11 @@ class PaiementsCsvImporter {
     const CSV_PAIEMENT_ID = 2;
     const CSV_FACTURE_ID = 3;
 
-    public function __construct(DocumentManager $dm, PaiementsManager $pm, FactureManager $fm) {
+    public function __construct(DocumentManager $dm, PaiementsManager $pm, FactureManager $fm, SocieteManager $sm) {
         $this->dm = $dm;
         $this->pm = $pm;
         $this->fm = $fm;
+        $this->sm = $sm;
     }
 
 
@@ -58,6 +61,13 @@ class PaiementsCsvImporter {
             if(!preg_match("/^[0-9]+$/", $data[self::CSV_REGLEMENT_ID])) {
                 continue;
             }
+
+            $societe = $this->sm->getRepository()->findOneByIdentifiantReprise($data[self::CSV_SOCIETE_ID]);
+            if (!$societe) {
+                $output->writeln(sprintf("<error>La societe n'a pas été trouvé %s </error>", $data[self::CSV_SOCIETE_ID]));
+                continue;
+            }
+
             $id = 'PAIEMENTS-' . (new \DateTime($data[self::CSV_PAIEMENT_DATE]))->format('Ymd');
             $paiements = $this->pm->getRepository()->findOneById($id);
             if (!$paiements) {
@@ -68,54 +78,172 @@ class PaiementsCsvImporter {
                 $this->dm->persist($paiements);
             }
 
-            $paiement = new Paiement();
-
-            $paiement->setIdentifiantReprise($data[self::CSV_REGLEMENT_ID]);
-            $paiement->setDatePaiement(new \DateTime($data[self::CSV_PAIEMENT_DATE]));
-            $paiement->setMontant($data[self::CSV_MONTANT]);
-            $paiement->setLibelle($data[self::CSV_LIBELLE]);
-            $moyens = array(
-                "1" => PaiementsManager::MOYEN_PAIEMENT_CHEQUE,
-                "2" => PaiementsManager::MOYEN_PAIEMENT_VIREMENT,
-                "3" => PaiementsManager::MOYEN_PAIEMENT_TRAITE,
-                "5" => PaiementsManager::MOYEN_PAIEMENT_CHEQUE
-            );
-            $paiement->setMoyenPaiement($moyens[$data[self::CSV_MOYEN_PAIEMENT]]);
-            $paiement->setTypeReglement(PaiementsManager::TYPE_REGLEMENT_FACTURE);
-            $paiement->setVersementComptable(true);
-
-            $facture = $this->fm->getRepository()->findOneBy(array('numeroFacture' => preg_replace("/[^[0-9]]*/", "", $data[self::CSV_LIBELLE])));
-
-            if($facture) {
+            $data[self::CSV_LIBELLE] = str_replace(array(" ET ", "->"), "/", $data[self::CSV_LIBELLE]);
+            $numerosFacture = preg_split("|/|", preg_replace("|[^0-9/]*|", "", $data[self::CSV_LIBELLE]));
+            $montant = $data[self::CSV_MONTANT]*1;
+            $paiement = null;
+            $facturesNonTrouves = array();
+            foreach($numerosFacture as $numeroFacture) {
+                $facture = $this->fm->getRepository()->findOneBy(array('numeroFacture' => $numeroFacture, 'societe' =>
+                $societe->getId()));
+                if(!$facture) {
+                    $facturesNonTrouves[] = $numeroFacture;
+                    continue;
+                }
+                $fCache = $this->dm->getUnitOfWork()->tryGetById($facture->getId(), $this->pm->getRepository()->getClassMetadata());
+                if($fCache) {
+                    $facture = $fCache;
+                }
+                $paiement = $this->createPaiement($data);
                 $paiement->setFacture($facture);
+                $paiement->setMontant($facture->getMontantTTC());
+                $montant -= $paiement->getMontant();
+                $paiements->addPaiement($paiement);
+
+                $facture->setMontantPaye($facture->getMontantPaye() + $paiement->getMontant());
+                $facture->updateRestantAPayer();
                 $factures[$facture->getId()] = $facture;
             }
 
-            $paiements->addPaiement($paiement);
+            if($paiement && !count($facturesNonTrouves) && round($montant, 2) != 0) {
+                //$output->writeln(sprintf("<comment>Le paiement est différent de celui des factures trouvés : %s</comment>", $data[self::CSV_REGLEMENT_ID].";".$data[self::CSV_LIBELLE].";".round($montant, 2)." € restant"));
+                $paiement->setMontant(round($paiement->getMontant() + round($montant, 2), 2));
+                $facture = $factures[$paiement->getFacture()->getId()];
+                $facture->setMontantPaye($facture->getMontantPaye() + round($montant, 2));
+                $facture->updateRestantAPayer();
+                $montant = 0;
+            }
+
+            if(round($montant, 2) != 0) {
+                $facture = $this->findFacture($societe, $data, round($montant, 2));
+                if($facture) {
+                    //$output->writeln(sprintf("<info>Facture trouvé : %s</info>", $data[self::CSV_REGLEMENT_ID].";".$data[self::CSV_LIBELLE].";".round($montant, 2)." € restant;".$facture->getId().";".$facture->getNumeroFacture().";".$facture->getDateFacturation()->format('Y-m-d').";".$facture->getMontantTTC()));
+                    $paiement = $this->createPaiement($data);
+                    $paiement->setFacture($facture);
+                    $paiement->setMontant(round($montant, 2));
+                    $paiements->addPaiement($paiement);
+
+                    $facture->setMontantPaye($facture->getMontantPaye() + round($montant, 2));
+                    $facture->updateRestantAPayer();
+                    $factures[$facture->getId()] = $facture;
+                    $montant -= round($montant, 2);
+                }
+            }
+
+            if(round($montant, 2) != 0 && count($facturesNonTrouves) > 0) {
+                foreach($facturesNonTrouves as $numeroFacture) {
+                    $facture = $this->fm->getRepository()->findOneBy(array('numeroFacture' => $numeroFacture));
+                    if(!$facture) {
+                        continue;
+                    }
+                    $fCache = $this->dm->getUnitOfWork()->tryGetById($facture->getId(), $this->pm->getRepository()->getClassMetadata());
+                    if($fCache) {
+                        $facture = $fCache;
+                    }
+                    $paiement = $this->createPaiement($data);
+                    $paiement->setFacture($facture);
+                    $paiement->setMontant($facture->getMontantTTC());
+                    $montant -= $paiement->getMontant();
+                    $paiements->addPaiement($paiement);
+
+                    $facture->setMontantPaye($facture->getMontantPaye() + $paiement->getMontant());
+                    $facture->updateRestantAPayer();
+                    $factures[$facture->getId()] = $facture;
+                }
+            }
+
+            if(round($montant, 2) != 0) {
+                $factures = $this->fm->getRepository()->findBy(array('societe' => $societe->getId()));
+                $facturePrecedente = null;
+                $facture = null;
+                foreach($factures as $facture) {
+                    $datePaiement = new \DateTime($data[self::CSV_PAIEMENT_DATE]);
+                    if($facture->getDateFacturation()->format('Y-m-d') > $datePaiement->format('Y-m-d')) {
+                        break;
+                    }
+                    $facturePrecedente = $facture;
+                }
+
+                if($facturePrecedente) {
+                    $facture = $facturePrecedente;
+                }
+
+                $paiement = $this->createPaiement($data);
+                $paiement->setFacture($facture);
+                $paiement->setMontant($facture->getMontantTTC());
+                $montant -= $paiement->getMontant();
+                $paiements->addPaiement($paiement);
+
+                $facture->setMontantPaye($facture->getMontantPaye() + $paiement->getMontant());
+                $facture->updateRestantAPayer();
+                $factures[$facture->getId()] = $facture;
+            }
+
+            if(round($montant, 2) != 0) {
+                $output->writeln(sprintf("<comment>Facture non trouvé pour ce montant : %s</comment>", $data[self::CSV_REGLEMENT_ID].";".$data[self::CSV_PAIEMENT_DATE].";".$data[self::CSV_LIBELLE].";".round($montant, 2)." € restant"));
+                echo implode(";", $data)."\n";
+            }
+
+
 
             $i++;
 
             if ($i >= 1000) {
-                $this->dm->flush();
-                $this->dm->clear();
+                /*$this->dm->flush();
                 foreach($factures as $f) {
                     $f->updateMontantPaye();
                     $f->updateRestantAPayer();
+                    $f->preUpdate();
                 }
                 $this->dm->flush();
                 $this->dm->clear();
-                $factures = array();
+                $factures = array();*/
                 $i = 0;
             }
         }
-        $this->dm->flush();
-        $this->dm->clear();
+        /*$this->dm->flush();
         foreach($factures as $f) {
             $f->updateMontantPaye();
             $f->updateRestantAPayer();
+            $f->preUpdate();
         }
         $this->dm->flush();
-        $this->dm->clear();
+        $this->dm->clear();*/
     }
 
+    protected function findFacture($societe, $data, $montant) {
+        $factures = $this->fm->getRepository()->findBy(array('societe' => $societe->getId()));
+        foreach($factures as $facture) {
+            $fCache = $this->dm->getUnitOfWork()->tryGetById($facture->getId(), $this->pm->getRepository()->getClassMetadata());
+            if($fCache) {
+                $facture = $fCache;
+            }
+            if($montant > $facture->getMontantAPayer()) {
+                continue;
+            }
+
+            return $facture;
+        }
+
+        return null;
+    }
+
+    protected function createPaiement($data) {
+        $paiement = new Paiement();
+
+        $paiement->setIdentifiantReprise($data[self::CSV_REGLEMENT_ID]);
+        $paiement->setDatePaiement(new \DateTime($data[self::CSV_PAIEMENT_DATE]));
+        $paiement->setLibelle($data[self::CSV_LIBELLE]);
+        $moyens = array(
+            "1" => PaiementsManager::MOYEN_PAIEMENT_CHEQUE,
+            "2" => PaiementsManager::MOYEN_PAIEMENT_VIREMENT,
+            "3" => PaiementsManager::MOYEN_PAIEMENT_TRAITE,
+            "5" => PaiementsManager::MOYEN_PAIEMENT_CHEQUE
+        );
+        $paiement->setMoyenPaiement($moyens[$data[self::CSV_MOYEN_PAIEMENT]]);
+        $paiement->setTypeReglement(PaiementsManager::TYPE_REGLEMENT_FACTURE);
+        $paiement->setVersementComptable(true);
+
+        return $paiement;
+    }
 }
