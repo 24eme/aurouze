4ddbea2fcbd18e7f7de82066c73c5dc89538ed4b
Gabriel Poma <gpoma@24eme.fr>
2022-05-12 17:49:18 +0200

    facture: fix 500 date paiement quand avoir

--- project/src/AppBundle/Document/Facture.php
+++ project/src/AppBundle/Document/Facture.php
@@ -438,7 +438,7 @@ class Facture implements DocumentSocieteInterface, FacturableInterface
               }
           }
         }
-        return $date->format('d/m/Y');
+        return ($date) ? $date->format('d/m/Y') : null;
     }
 
     /**
