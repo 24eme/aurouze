49921306a00b4cbb69a33ee51bdb4cd525229efa
chachathecool <charlene.connorshg@gmail.com>
2024-07-26 17:59:27 +0200

    Formulaire de modification du titre d'un document

---
+++ project/src/AppBundle/Type/AttachementNameType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Type;
+
+use AppBundle\Document\Attachement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ODM\MongoDB\DocumentManager;
+
+
+
+
+class AttachementNameType extends AbstractType {
+
+    protected $dm;
+
+    public function __construct(DocumentManager $documentManager) {
+        $this->dm = $documentManager;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => Attachement::class,
+            'allow_extra_fields' => true,
+        ));
+    }
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('titre', TextType::class, array(
+                    'label' => 'Modifier le titre',
+                ))
+            ->add('save', SubmitType::class, array(
+            'label' => 'Sauvegarder'
+            ))
+            ->getForm();
+
+        }
+}
+
